15c818cd24ef080acda495f23e1da807a55730f0
Miguel Julio <mj@example.com>
2022-06-15 20:39:07 +0200

    Realizado Commit

---
+++ src/Prueba/Declaraciones_Operdores.java
@@ -0,0 +1,16 @@
+package Prueba;
+
+public class Declaraciones_Operdores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	final int c=5;
+	
+	c = 6; 
+	
+	System.out.println(c);
+	
+	}
+
+}
---
+++ src/Prueba/Variables.java
@@ -0,0 +1,20 @@
+package Prueba;
+
+public class Variables {
+
+	public static void main(String[] args) {
+	
+		int edad =35;
+		
+		
+		
+		System.out.println(edad);
+		
+		edad = 75;
+		
+		System.out.println(edad);		
+		
+		
+	}
+
+}
